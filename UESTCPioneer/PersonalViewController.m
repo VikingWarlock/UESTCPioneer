9c4df4a4eb472a911d8e42dbfee4c2f3f0c0d649
ZhongJohn <zhangzhong0512@163.com>
2014-03-19 14:34:20 +0800

    请无视。。我还是在测试

--- UESTCPioneer/PersonalViewController.m
+++ UESTCPioneer/PersonalViewController.m
@@ -3,7 +3,7 @@
 //  UESTCPioneer
 //
 //  Created by Sway on 14-3-5.
-//  Copyright (c) 2014年 Sway. All rights reserved.
+//  Copyright (c) 2014年 Sway. All rights reserved
 //
 
 #import "PersonalViewController.h"
